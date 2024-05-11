5aa289747b2fc3b40209140767f6504168d269f4
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-05-11 22:24:01 +0300

    Guess My Number

---
+++ README.md
@@ -0,0 +1,35 @@
+## [04 - Guess My Number]
+
+Guess My Number is a simple web-based guessing game hosted on Netlify. Player guesses a randomly generated number within a specified range.After player make their guesses into a text field and receive feedback on whether their guess is too high, too low, or correct. The game continues until the player guesses the correct number or the score is end. You can try by clicking <strong>[Guess My Number](https://guess-my-number-gldn.netlify.app/)</strong>
+
+#### Features:
+
+- Random number generation within a specific range.
+- User input for guessing the number.
+- Feedback on each guess (too high, too low, correct).
+- Play again functionality to start a new game.
+
+#### Technologies Used:
+
+- HTML, CSS, JavaScript for front-end development.
+- Netlify for hosting the web application.
+
+ <br/> 
+   <img src="./ReadME__img/04 - Guess My Number/guessMyNumber.png" alt="Guess My Number">
+<br/>
+
+### What did I learn while coding this project ?
+
+> #
+>
+> - _what is DOM and DOM Manipulation ,_
+> - _connection between DOM and Javascript,_
+> - _usage of document.querySelector() function ,_
+> - _difference between value() and textContent() function ,_
+> - _addEventListener('event', functionExpression(){}) ,_
+> - _querySelector() returns an object, textContent and value return string ,_
+> - _Math.random(), Math.trunc() functions ,_
+> - _how to manipulate element's style with javascript ,_
+> - _how to refactor code ,_
+>
+> #
---
+++ script.js
@@ -0,0 +1,108 @@
+'use strict';
+
+const btnCheck = document.querySelector('.check');
+const messageBox = document.querySelector('.message');
+const checkBox = document.querySelector('.guess');
+const secretNumBox = document.querySelector('.number');
+const scoreBox = document.querySelector('.score');
+const body = document.querySelector('body');
+const againBtn = document.querySelector('.again');
+const highScoreBox = document.querySelector('.highscore');
+
+//0-19 arasında random satı üretirken 1 ekleyerek 1-20 arasında olmasını sağladık
+let secretNum = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+// console.log(secretNum);
+
+const displayMessage = function (message) {
+  messageBox.textContent = message;
+};
+
+btnCheck.addEventListener('click', function () {
+  const checkVal = Number(checkBox.value);
+
+  if (!checkVal || checkVal > 20 || checkVal < 0) {
+    displayMessage('No number 🤡');
+  } else if (checkVal === secretNum) {
+    displayMessage('Congratulations 🎉');
+    body.style.backgroundColor = '#60b347';
+    secretNumBox.textContent = secretNum;
+
+    if (score > highScore) {
+      highScore = score;
+      highScoreBox.textContent = highScore;
+    }
+
+    secretNumBox.style.width = '30rem';
+  } else if (checkVal !== secretNum) {
+    displayMessage(checkVal > secretNum ? 'Too high 📈' : 'Too low 📉');
+
+    if (score > 1) {
+      score--;
+    } else {
+      score = 0;
+      displayMessage('You lost the game');
+    }
+  }
+  //  else if (checkVal > secretNum) {
+  //   messageBox.textContent = 'Too high 📈';
+
+  //   if (score > 1) {
+  //     score--;
+  //   } else {
+  //     score = 0;
+  //     messageBox.textContent = 'You lost the game';
+  //   }
+  // } else if (checkVal < secretNum) {
+  //   messageBox.textContent = 'Too low 📉';
+
+  //   if (score > 1) {
+  //     score--;
+  //   } else {
+  //     score = 0;
+  //     messageBox.textContent = 'You lost the game';
+  //   }
+  // }
+
+  scoreBox.textContent = score;
+  // console.log(checkVal, typeof checkVal);
+});
+
+againBtn.addEventListener('click', function () {
+  score = 20;
+  secretNum = Math.trunc(Math.random() * 20 + 1);
+  console.log(secretNum);
+  scoreBox.textContent = score;
+  secretNumBox.textContent = '?';
+  messageBox.textContent = ' Start guessing...';
+  checkBox.value = '';
+  body.style.backgroundColor = '#222';
+  secretNumBox.style.width = '15rem';
+});
+
+//console.log(btnCheck, typeof btnCheck);
+
+/*
+//----- Selecting and Manipulating Elements -----
+
+const number = document.querySelector('.number');
+number.textContent = 24;
+console.log(number.textContent);
+
+document.querySelector('.message').textContent = 'Correct number 🎉';
+
+const inputNum = (document.querySelector('.left .guess').value = 25);
+const inputNum2 = (document.querySelector('.left .guess').textContent = 30);
+
+console.log(inputNum);
+console.log(inputNum2);
+*/
+
+/*
+//----- DOM -----
+const btn = document.querySelector('.btn.check');
+const btnText = document.querySelector('.btn.check').textContent;
+console.log(btn);
+console.log(btnText);
+*/
